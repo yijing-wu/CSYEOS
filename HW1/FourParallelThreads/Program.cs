679a68257e98ef17eae3a3db23e972ffae946eca
yijing-wu <yijingwu.w@gmail.com>
2023-02-26 14:02:09 -0800

    Assignment 1

---
+++ HW1/FourParallelThreads/Program.cs
@@ -0,0 +1,41 @@
+﻿// 1. Write code in any language which will create 4 threads and run them in parallel.
+
+internal class Program {
+
+    public static Object objLock = new Object();
+
+    public static int totalValue = 0;
+    
+    private static void Main(string[] args) {
+
+        Thread thOne = new Thread(addValue);
+        thOne.Name = "ThreadOne";
+        thOne.Start(1);
+
+        Thread thTwo = new Thread(addValue);
+        thTwo.Name = "ThreadTwo";
+        thTwo.Start(1);
+
+        Thread thThree = new Thread(addValue);
+        thThree.Name = "ThreadThree";
+        thThree.Start(1);
+
+        Thread thFour = new Thread(addValue);
+        thFour.Name = "ThreadFour";
+        thFour.Start(1);
+
+    }
+
+    static void addValue(Object obj) {
+        int value = (int) obj;
+        while(true) {
+            lock(objLock) {
+                Console.WriteLine(Thread.CurrentThread.Name + " add value: " + value);
+                Thread.Sleep(200);
+                totalValue += value;
+                Console.WriteLine("Total Value = " + totalValue);
+            }
+        } 
+
+    }
+}
---
+++ HW1/MultiplyValue/Program.cs
@@ -0,0 +1,68 @@
+﻿// 2. Write code in any language which will write code to 
+// Multiply every value in a array with Random value 
+// between 0.1 to 0.9 Calculate the sum of the array 
+// in parallel and serial and compute the time difference. 
+using System.Diagnostics;
+
+internal class Program {
+
+    static double[] values = new double[5000000];
+    
+    static double[] portionResult = new double[4];
+    
+    static int portionSize = values.Length/4;
+
+    private static void Main(string[] args) {
+        genereateValues();
+
+        Stopwatch watch = new Stopwatch();
+        watch.Start();
+        double result = 0;
+        for (int i = 0; i < values.Length; i++) {
+            result += values[i];
+        }
+        watch.Stop();
+        Console.WriteLine("Total Sum = " + result);
+        Console.WriteLine("Time elapsed = " + watch.Elapsed);
+
+        watch.Reset();
+
+        watch.Start();
+        Thread[] threads = new Thread[4];
+        for(int i = 0; i < threads.Length; i++) {
+            threads[i] = new Thread(calculateSum);
+            threads[i].Start(i);
+        }
+
+        for(int i = 0; i < threads.Length; i++) {
+            threads[i].Join();
+        }
+
+        double sumParallel = 0;
+        for(int i = 0; i < portionResult.Length; i++) {
+            sumParallel += portionResult[i];
+        }
+        watch.Stop();
+        Console.WriteLine("Total Sum = " + sumParallel);
+        Console.WriteLine("Time elapsed = " + watch.Elapsed);
+
+    }
+
+    static void genereateValues() {
+        Random rand = new Random();
+        for(int i = 0; i < values.Length; i++) {
+            // generate values and multuply
+            values[i] = rand.NextDouble() * (0.9 - 0.1) + 0.1;
+        }
+    }
+
+    static void calculateSum(object obj) {
+        int portion = (int) obj;
+        double sum = 0;
+        for(int i = portion * portionSize; i < portion * portionSize + portionSize; i++) {
+            sum += values[i];
+        }
+        portionResult[portion] = sum;
+    }
+
+}
